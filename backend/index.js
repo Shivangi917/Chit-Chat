52c1e1db4118a4275d018722da1ab9205b60da78
Shivangi Shree <shivangishree020@gmail.com>
2025-03-29 23:56:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const connectDB = require('./db/db.db');
 const chatRoute = require('./route/chat.route');
 
 const app = express();
@@ -11,7 +10,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
-connectDB();
 app.use('/api', chatRoute);
 
 
